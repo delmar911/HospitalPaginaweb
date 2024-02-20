959f5538935f618b145af89e1168ac1e6d2bbcff
mayra tamayo <mairatp2005@gmail.com>
2024-02-20 15:42:48 -0500

    nueva actualizacion front_end, y en el js arregle unas cosas de la validacion, en el back_end arregle una importacion del id que estaba mal importada

--- back_end/hospitalWeb/src/main/java/com/sena/hospitalWeb/model/ingreso.java
+++ back_end/hospitalWeb/src/main/java/com/sena/hospitalWeb/model/ingreso.java
@@ -3,7 +3,7 @@ package com.sena.hospitalWeb.model;
 import java.time.LocalDateTime;
 
 
-import org.springframework.data.annotation.Id;
+import jakarta.persistence.Id;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
--- back_end/hospitalWeb/src/main/java/com/sena/hospitalWeb/model/paciente.java
+++ back_end/hospitalWeb/src/main/java/com/sena/hospitalWeb/model/paciente.java
@@ -1,6 +1,6 @@
 package com.sena.hospitalWeb.model;
 
-import org.springframework.data.annotation.Id;
+import jakarta.persistence.Id;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
